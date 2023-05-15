7c673e77f8474b739f3cc9dfe9028e9a4954fe2d
TheComet <alex.murray@gmx.ch>
2023-05-16 01:05:43 +0200

    Using vendored SDL and SDL_image instead of find_package()

--- src/controls.c
+++ src/controls.c
@@ -5,12 +5,12 @@
 void
 input_init(struct input* i)
 {
-    memset(i, 0, sizeof * i);
+    memset(i, 0, sizeof *i);
 }
 
 /* ------------------------------------------------------------------------- */
 void
 controls_init(struct controls* c)
 {
-    memset(c, 0, sizeof * c);
+    memset(c, 0, sizeof *c);
 }
