a3f94dfc546d1b456f8f9838e031fc49d2c4f5a9
TheComet <alex.murray@gmx.ch>
2023-05-31 00:34:12 +0200

    Working on rollback

--- src/controls.c
+++ src/controls.c
@@ -10,9 +10,8 @@ input_init(struct input* i)
 
 /* ------------------------------------------------------------------------- */
 void
-controls_init(struct controls* c, uint16_t frame_number)
+controls_init(struct controls* c)
 {
-    c->frame_number = frame_number;
     c->angle = 0;
     c->speed = 0;
     c->action = CONTROLS_ACTION_NONE;
